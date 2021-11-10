0345f2b42b8d32f206a93c4a99da412d6915dc1d
Opilane TTHK <opilane@tthk.ee>
2021-11-10 12:27:54 +0200

    Add project files.

---
+++ Omad_funktsioonid/module1.py
@@ -0,0 +1,169 @@
+def arithmetic(a: float,b:float,c:str):
+    """Lihtne kalkulaator
+    + - liitmine
+    - - lahutamine
+    * - korrutamine
+    / - jagamine
+    :param float a: Esimene arv
+    :param float b: Teine arv
+    :param float c: Aritmeetilene tehing
+    :rtype float:
+    """
+    if c=="+":
+        r=a+b
+    elif c=="-":
+        r=a-b 
+    elif c=="*":
+        r=a*b
+    elif c=="/":
+        if b!=0:
+            r=a/b
+        else:
+            print("Div0")
+            r=0.0
+    else:
+        print("Viga")
+        r=0.0
+    return r
+
+def is_year_leap(aasta: int):
+    """Liigaasta
+    Tagastab true kui aasta on liigaasta ja False kui ei ole
+    :param int aasta: Aasta number
+    :rtype bool: Funktsiooni vastus logilises formaadis
+    """
+    if aasta%4==0:
+        vastus=True
+    else:
+        vastus=False
+    return vastus
+
+def square(a):
+    """Ruudu külg
+    Annab vastust ruudu pindala,diogonaal,ümbermõõt
+    :param int külg: Kui pikk külg
+    :rtype float:
+    """
+    p = 4*a
+    s = a*a
+    d = (a**2) / 2
+    k = (p, s, d)
+    return k
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
